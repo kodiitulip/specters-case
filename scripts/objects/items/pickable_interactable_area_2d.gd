8af7dd8bd31a1434ce93d517519eda69fc095da2
kodiitulip <kodii.tulip@gmail.com>
2025-10-20 00:29:28 -0300

    docs: adding documentation to the new nodes

--- scripts/objects/items/pickable_interactable_area_2d.gd
+++ scripts/objects/items/pickable_interactable_area_2d.gd
@@ -1,10 +1,23 @@
 class_name PickableInteractableArea2D
 extends InteractableArea2D
+## PickableInteractableArea2D
+##
+## This [InteractableArea2D] allows an item with [ItemData] to be picked up and
+## sent to the [InventoryInterface]
 
+## The data of this item
 @export var item_data: ItemData
+## Tell if the item should desapear after being picked up
 @export var finite: bool = true
-@export var free_target: Node
+## The Node this area should [code]queue_free()[/code] in case of being finite.
+## If [code]null[/code], deafults to [code]queue_free()[/code] the area itself
+@export var free_target: Node = null
 
+func _ready() -> void:
+	assert(item_data != null, "[code]ItemData[/code] must not be null")
+
+
+## Called the first frame that the interaction starts
 func on_interact_started() -> void:
 	InventoryInterface.instance.pickup_item(item_data)
 	if not finite:
@@ -14,9 +27,11 @@ func on_interact_started() -> void:
 	queue_free()
 
 
+## Called for every frame that the interaction is happening
 func on_interacting() -> void:
 	pass
 
 
+## Called on the last frame that the interaction happens
 func on_interact_ended() -> void:
 	pass
