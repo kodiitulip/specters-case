c36d7b8d1fe77dd94b22419a28336cca1d836d3d
kodiitulip <kodii.tulip@gmail.com>
2025-10-20 00:29:28 -0300

    docs: adding documentation to the new nodes

--- scripts/objects/items/overlay_interactable_area_2d.gd
+++ scripts/objects/items/overlay_interactable_area_2d.gd
@@ -1,18 +1,25 @@
 class_name OverlayInteractableArea2D
 extends InteractableArea2D
+## OverlayInteractableArea2D
+##
+## This [InteractableArea2D] allows an Item to open an overlay on the users
+## screen. The scene file for the overlay must be set!
 
+## The scene used to instanciate the overlay
 @export var overlay_scene: PackedScene
 
 
+## Called the first frame that the interaction starts
 func on_interact_started() -> void:
 	assert(overlay_scene != null, "[code]overlay_scene[/code] must not be null")
 	var overlay: Node = overlay_scene.instantiate()
 	OverlayLayer.change_overlay_to(overlay)
 
-
+## Called for every frame that the interaction is happening
 func on_interacting() -> void:
 	pass
 
 
+## Called on the last frame that the interaction happens
 func on_interact_ended() -> void:
 	pass
