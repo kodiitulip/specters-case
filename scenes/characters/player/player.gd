1104d8730f86d0d2d98935f9d99fc24cbc8963cb
Davi <davi.souza.comp@gmail.com>
2025-12-24 13:48:10 -0300

    Adding black and white filter

--- scenes/characters/player/player.gd
+++ scenes/characters/player/player.gd
@@ -1,34 +1,30 @@
 extends CharacterBody2D
 
-# Defining some player variables
 @export var speed: float = 150.0
-
-var target_pos: Vector2 # Target position to move
 var _mouse_busy: bool = false: set = _set_mouse_busy
 
 func _ready() -> void:
-	target_pos = global_position
 	GlobalSignalBus.mouse_busy.connect(_set_mouse_busy)
 
-# Function that sets the target position
-func _unhandled_input(event: InputEvent) -> void:
-	if _mouse_busy:
-		return
-	if event.is_action_pressed(&"left_mouse_button"):
-		target_pos = get_global_mouse_position()
-	
-# Making player move
 func _physics_process(delta: float) -> void:
-	if not target_pos:
+	if _mouse_busy:
+		velocity = Vector2.ZERO
+		move_and_slide()
 		return
-	global_position = global_position.move_toward(target_pos, speed * delta)
-	#var direction: Vector2 = (target_pos - global_position)
-	#if direction.length() > 1:
-		#velocity = direction.normalized() * speed
-	#else:
-		#velocity = Vector2.ZERO
-	#move_and_slide()
 
+	var direction := Vector2.ZERO
+
+	if Input.is_action_pressed("ui_right") or Input.is_action_pressed("d"):
+		direction.x += 1
+	if Input.is_action_pressed("ui_left") or Input.is_action_pressed("a"):
+		direction.x -= 1
+	if Input.is_action_pressed("ui_down") or Input.is_action_pressed("s"):
+		direction.y += 1
+	if Input.is_action_pressed("ui_up") or Input.is_action_pressed("w"):
+		direction.y -= 1
+
+	velocity = direction.normalized() * speed
+	move_and_slide()
 
 func _set_mouse_busy(value: bool) -> void:
 	_mouse_busy = value
